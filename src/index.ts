9a62f75e0334b346027acf5242b522991c7d76d6
Dotan Simha <dotansimha@gmail.com>
2019-10-05 16:49:29 +0300

    added basic example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { GraphQLClient } from 'graphql-request';
+import { getSdk } from './sdk';
+
 async function main() {
-  console.log('Hello World!');
+  const client = new GraphQLClient('https://countries.trevorblades.com/');
+  const sdk = getSdk(client);
+  const { continents } = await sdk.continents();
+  
+  console.log(`GraphQL data:`, continents);
 }
 
 main().catch(e => {
